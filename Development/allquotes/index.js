5f50c95ae12d7ea85ac59093a26a490df010cbd3
seron <seroncj@gmail.com>
2022-09-15 13:44:15 +0300

    Resubmission

--- Development/allquotes/index.js
+++ Development/allquotes/index.js
@@ -49,3 +49,20 @@ fetch("https://type.fit/api/quotes")
 
   }
   });
+
+  const togglePassword = document.getElementById('togglePassword');
+const password = document.getElementById('password');
+
+togglePassword.addEventListener('click', function (e) {
+    let type = password.getAttribute('type');
+    if (type === 'password'){
+        type = 'text'
+    }
+    else{
+        type = 'password'
+    }
+    password.setAttribute('type', type);
+    this.classList.toggle('fa-eye-slash');
+});
+  
+
